cdc59ee3f40ec339828bea87d3f24458104be7d5
cync48 <ridhoperdana@ymail.com>
2017-09-08 23:46:12 +0700

    Add new target ui

--- app/src/main/java/net/ridhoperdana/asumu/fragment/ItemHomeFragment.java
+++ app/src/main/java/net/ridhoperdana/asumu/fragment/ItemHomeFragment.java
@@ -1,16 +1,20 @@
 package net.ridhoperdana.asumu.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.ridhoperdana.asumu.AddNewTargetActivity;
 import net.ridhoperdana.asumu.R;
 
-public class ItemHomeFragment extends Fragment {
+public class ItemHomeFragment extends Fragment implements View.OnClickListener{
 
     public static ItemHomeFragment newInstance() {
         ItemHomeFragment fragment = new ItemHomeFragment();
@@ -25,6 +29,20 @@ public class ItemHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_item_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_item_home, container, false);
+
+        FloatingActionButton buttonAddTarget = (FloatingActionButton)view.findViewById(R.id.add_target_button);
+        buttonAddTarget.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.add_target_button)
+        {
+            Log.d("berhasil", "add");
+            Intent intent = new Intent(getActivity(), AddNewTargetActivity.class);
+            startActivity(intent);
+        }
     }
 }
