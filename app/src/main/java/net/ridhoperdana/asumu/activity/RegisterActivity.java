40f8f082e3fbc100860d40273ac9d84408d783ba
Ronald Gunawan R <geronald10@gmail.com>
2017-09-08 22:43:51 +0700

    Add Login Activity + Logic

---
+++ app/src/main/java/net/ridhoperdana/asumu/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package net.ridhoperdana.asumu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.ridhoperdana.asumu.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/net/ridhoperdana/asumu/utility/NetworkUtils.java
@@ -0,0 +1,11 @@
+package net.ridhoperdana.asumu.utility;
+
+public class NetworkUtils {
+
+    public final static String REGISTER_ASUMU =
+            "http://172.20.10.3:1234/Asumu/Services/register";
+
+    public final static String LOGIN_ASUMU =
+            "http://172.20.10.3:1234/Asumu/Services/login";
+
+}
